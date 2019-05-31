02dc998480930d2c42fb8ff44cdddfcd11a5a2bc
Ben Vars <spyder21496@aim.com>
2019-05-30 23:54:04 -0400

    created beginner of base component slider

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TouchableHighlight,
+	ScrollView,
+	SafeAreaView,
+	Dimensions
+} from 'react-native';
+
+import ComponentSlider from './ComponentSlider.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	renderImages = (image, index) => {
+		const imageObject = typeof image === 'string' ? { uri: image } : image;
+		return <Image key={index} source={imageObject} style={styles.image} />;
+	};
+
+	render() {
+		const images = [
+			'https://placeimg.com/640/640/nature',
+			'https://placeimg.com/640/640/people',
+			'https://placeimg.com/640/640/animals',
+			'https://placeimg.com/640/640/beer'
+		];
+
+		return (
+			<SafeAreaView style={styles.container}>
+				<View style={{ height: 300 }}>
+					<ComponentSlider
+						data={images}
+						renderComponent={this.renderImages}
+						style={styles.componentSlider}
+					/>
+				</View>
+			</SafeAreaView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#F5FCFF'
+	},
+	componentSlider: {
+		backgroundColor: '#000',
+		height: 150,
+		width: '100%'
+	},
+	content1: {
+		width: '100%',
+		height: 50,
+		marginBottom: 10,
+		backgroundColor: '#000',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	content2: {
+		width: '100%',
+		height: 100,
+		marginTop: 10,
+		backgroundColor: '#000',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	contentText: { color: '#fff' },
+	buttons: {
+		zIndex: 1,
+		height: 15,
+		marginTop: -25,
+		marginBottom: 10,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'row'
+	},
+	button: {
+		margin: 3,
+		width: 15,
+		height: 15,
+		opacity: 0.9,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	buttonSelected: {
+		opacity: 1,
+		color: 'red'
+	},
+	customSlide: {
+		backgroundColor: 'green',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	customImage: {
+		width: 100,
+		height: 100
+	},
+	image: {
+		width: Dimensions.get('window').width,
+		height: '100%'
+	}
 });
